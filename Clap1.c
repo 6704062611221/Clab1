bde63d54093ec65ad0693f8200602c06d9122466
6704062611221 <s6704062611221@email.kmutnb.ac.th>
2024-11-28 14:17:12 +0700

    clab1

---
+++ Clap1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int num1 = 2;
+	int num2 = 3;
+	if (num1>num2) {
+		printf("num1 greater than num2");
+	}
+	if (num2>num1){
+		printf("num2 greater than num1");
+	}
+}
